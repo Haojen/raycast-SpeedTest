f18c45d699b31f4e96993c4f622e2bc8f974536c
Haojen <haojenma@gmail.com>
2022-03-08 20:21:40 +0800

    Add Report ID

--- src/detail-content.ts
+++ src/detail-content.ts
@@ -8,6 +8,7 @@ import {
 import { environment } from '@raycast/api'
 // {"id":13623,"host":"speedtest.singnet.com.sg","port":8080,"name":"Singtel","location":"Singapore","country":"Singapore","ip":"165.21.70.1"}
 // {"internalIp":"192.168.2.106","name":"en0","macAddr":"F4:D4:88:80:6B:DB","isVpn":false,"externalIp":"222.129.130.228"}
+// "result":{"id":"4b735fec-11ec-4b2e-8fbd-7eb80dd72c56","url":"https://www.speedtest.net/result/c/4b735fec-11ec-4b2e-8fbd-7eb80dd72c56","persisted":true}
 export default (
   log?: ISpeedLog,
   testStart?: ISpeedTestStart,
@@ -53,7 +54,7 @@ Host: ${ testStart?.server.host }
 >>>
 Date: 2022:02:02
 
-Report ID: xxxxxx
+Report ID: ${ result?.result.id ? '*' + result?.result.id + '*' : 'Report not created' }
 >>>
 ---
 ## About
